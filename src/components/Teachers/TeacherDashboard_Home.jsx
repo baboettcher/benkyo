457640665f5f2e68cd263ed438329a7b6f6646fb
Adam Boettcher <baboettcher@gmail.com>
2018-04-29 16:37:39 -0700

    teacher name, last login, image added to teacherDash home

--- src/components/Teachers/TeacherDashboard_Home.jsx
+++ src/components/Teachers/TeacherDashboard_Home.jsx
@@ -1,17 +1,42 @@
-import React from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { Menu, MenuItem, Dropdown, Container } from 'semantic-ui-react';
 
 
-export default function Assessments() {
-  return (
-    <div>
-      <ul>
-        <h2>Benkyo Teacher Dashboard</h2>
-        <li>Teacher name:</li>
-        <li>Last Login:</li>
-        <li>Active Assessments:</li>
-        <li>Assessment set:</li>
-      </ul>
+@connect((store) => {
+  return {
+    user_cred: store.authentication.user_cred
+  }
+})
 
-    </div>
-  )
+
+class HomePage extends Component {
+
+  render(){
+
+    const { user_cred } = this.props;
+    console.log("user_cred", user_cred);
+
+    return (
+      <div>
+        <ul>
+          <h2>Benkyo Teacher Dashboard</h2>
+          <img src={user_cred.photoURL} alt="placeholder" height="42" width="42" />
+          <li>Teacher name: {user_cred.displayName}</li>
+          <li>Last Login:{user_cred.metadata.lastSignInTime}</li>
+          <li>Current class:</li>
+          <li>Number of students:</li>
+          <li>Active Assessments:</li>
+          <li>Assessment set:</li>
+        </ul>
+      </div>
+    )
+  }
 }
+    
+export default HomePage;
+    
+    
+    
+      
